ff73210cc274b9543a44fd444ff24257db48ac5c
Marcelo <m.lagazzi@hotmail.com>
2026-06-23 22:44:50 -0300

    Adiciona layout base com sidebar fixa e navegação vertical

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,30 +7,37 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased bg-gray-100">
+
+        <div class="flex min-h-screen">
+
+            {{-- Sidebar --}}
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            {{-- Conteúdo principal --}}
+            <div class="flex-1 flex flex-col">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                {{-- Cabeçalho da página (opcional) --}}
+                @isset($header)
+                    <header class="bg-white shadow">
+                        <div class="px-6 py-4">
+                            {{ $header }}
+                        </div>
+                    </header>
+                @endisset
+
+                {{-- Conteúdo --}}
+                <main class="flex-1 p-6">
+                    {{ $slot }}
+                </main>
+
+            </div>
         </div>
+
     </body>
 </html>
